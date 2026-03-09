f39d42d27a71b49cd402a5a0c29dccc66c7e31b8
Vedantha-V-V <vernekar.vedantha2004@gmail.com>
2026-03-09 23:08:33 +0530

    Edit and add functionality done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import render_template
-from parser import fetch_articles,fetch_article_by_id
+from flask import render_template,request
+from parser import fetch_articles,fetch_article_by_id,add_post,edit_post
 
 app = Flask(__name__)
 
@@ -9,19 +9,34 @@ def home_page():
     articles_list = fetch_articles()
     return render_template("home.html",articles_list=articles_list)
 
-@app.route("/article/<id>")
+@app.route("/article/<id>",methods=['GET','POST'])
 def article_page(id):
+    print(id)
+    print(request.method)
+    if(request.method == 'POST'):
+        title = request.form.get('title')
+        date = request.form.get('date')
+        desc = request.form.get('description')
+        edit_post(id,title,date,desc)
     article = fetch_article_by_id(id)
     return render_template("article.html",article=article)
 
-@app.route("/admin")
+@app.route("/admin",methods=['GET','POST'])
 def admin_page():
+    if(request.method == 'POST'):
+        title = request.form.get('title')
+        date = request.form.get('date')
+        desc = request.form.get('description')
+        add_post(title,date,desc)
     articles_list = fetch_articles()
     return render_template("admin.html",articles_list=articles_list)
 
+
 @app.route("/edit/<id>")
 def edit_article(id):
-    return 'Index Page'
+    article = fetch_article_by_id(id)
+    print(article)
+    return render_template("edit.html",article=article)
 
 @app.route("/new")
 def add_article():
--- parser.py
+++ parser.py
@@ -21,7 +21,6 @@ def fetch_article_by_id(id):
     directory = './articles'
     paths = os.listdir(directory)
     article_path = f"./articles/AT{id}.json"
-    print(article_path)
     try:
         with open(article_path, 'r', encoding='utf-8') as file:
             article = json.load(file)
@@ -32,7 +31,40 @@ def fetch_article_by_id(id):
             print("Error: Failed to decode JSON from the file. Check for invalid JSON syntax.")
     return ""
 
+def add_post(title,date,desc):
+    article = {}
+    files = os.listdir('articles')
+    id = int(files[len(files)-1][2:-5])+1
+    article["id"] = id
+    article['title'] = title
+    article['date'] = date
+    article['description']  = desc
+    post = f"./articles/AT{id}.json"
+    try:
+        with open(post, 'w', encoding='utf-8') as file:
+            json.dump(article,file)
+    except FileNotFoundError:
+            print("Article {article} not found.")
+    except json.JSONDecodeError:
+            print("Error: Failed to decode JSON from the file. Check for invalid JSON syntax.")
 
+def edit_post(id,title,date,desc):
+    post = f"./articles/AT{id}.json"
+    try:
+        with open(post, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        data['title'] = title
+        data['date'] = date
+        data['description'] = desc
+        with open(post, 'w', encoding='utf-8') as file:
+            json.dump(data,file)
+    except FileNotFoundError:
+            print("Article {article} not found.")
+    except json.JSONDecodeError:
+            print("Error: Failed to decode JSON from the file. Check for invalid JSON syntax.")
+
+def delete_post(id):
+    pass
 
 
 def main():
